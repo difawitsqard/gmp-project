ca0ebdcfe096956eaf59378691929b09f2452a56
difawitsqard <difawitsqard@gmail.com>
2025-07-30 16:13:40 +0700

    change path forgot pass

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -2,7 +2,7 @@
 import * as Yup from "yup";
 import { Head, Link, useForm } from "@inertiajs/vue3";
 import { ref } from "vue";
-import TextInput from "@/Components/TextInput.vue";
+import TextInput from "@/components/TextInput.vue";
 
 defineProps({
     status: String,
